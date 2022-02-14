85ba82c1eaf7b67a506f1f22f07499ef6e68eeb3
Nag <nagcloudlab@gmail.com>
2022-02-14 11:04:32 +0530

    recap & auth intro

--- play-with-Hasura/functions/src/index.ts
+++ play-with-Hasura/functions/src/index.ts
@@ -1,17 +1,29 @@
 
 
 import * as functions from "firebase-functions";
+
+
+import * as admin from "firebase-admin";
 import { notifyAboutCommentHandler } from "./notifyAboutComment";
-import { createAccountHandler } from "./createAccount";
-import { getAccountHandler } from "./getAccount";
+import { createUserHandler } from "./createUser";
+import { getProfileHandler } from "./getProfile";
+import initApolloServer from "./remoteSchema";
+
+const adminConfig = JSON.parse(process.env.FIREBASE_CONFIG as string);
+if (functions.config()?.hasura?.env === "local") {
+    console.log("local")
+    const serviceAccount = require("../serviceAccountKey.json");
+    adminConfig.credential = admin.credential.cert(serviceAccount);
+}
+admin.initializeApp(adminConfig);
 
 
-// function-1
 export const notifyAboutComment = functions.https.onRequest(
     notifyAboutCommentHandler
 );
-// function-3
-export const createAccount = functions.https.onRequest(createAccountHandler);
-
-// function-3
-export const getAccount = functions.https.onRequest(getAccountHandler);
+export const createUser = functions.https.onRequest(createUserHandler);
+export const getProfile = functions.https.onRequest(getProfileHandler);
+// GraphQL
+export const userProfile = functions.https.onRequest(
+    initApolloServer.createHandler()
+);
